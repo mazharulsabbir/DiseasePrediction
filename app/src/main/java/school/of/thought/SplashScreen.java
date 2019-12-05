2cc93d4b050a871bd6ea0d902f2f934054368068
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-12-05 19:40:33 +0600

    Initial commit

---
+++ app/src/main/java/school/of/thought/SplashScreen.java
@@ -0,0 +1,19 @@
+package school.of.thought;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            /*todo: open activity which is the main activity*/
+        }, 300);
+    }
+}
