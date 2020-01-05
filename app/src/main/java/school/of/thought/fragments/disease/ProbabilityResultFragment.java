c66f51b16090edd002f622e86a8628462c9ca4bd
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-01-05 15:43:09 +0600

    disease result with doctor list

--- app/src/main/java/school/of/thought/fragments/disease/ProbabilityResultFragment.java
+++ app/src/main/java/school/of/thought/fragments/disease/ProbabilityResultFragment.java
@@ -2,6 +2,7 @@ package school.of.thought.fragments.disease;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +11,29 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import school.of.thought.R;
+import school.of.thought.adapter.DoctorListAdapter;
 import school.of.thought.model.Dengue;
+import school.of.thought.model.DoctorRegistrationModel;
 import school.of.thought.utils.Utils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProbabilityResultFragment extends Fragment {
-
+    private static final String TAG = "ProbabilityResult";
     private Dengue dengue;
 
     public static ProbabilityResultFragment newInstance(Dengue dengue) {
@@ -53,7 +67,48 @@ public class ProbabilityResultFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         TextView result = view.findViewById(R.id.result);
+        String string;
 
-        result.setText(String.valueOf(dengue.getDengue()));
+        if (Integer.parseInt(dengue.getDengue()) == 0)
+            string = "Negative";
+        else string = "Positive";
+
+        result.setText(string);
+
+        doctorLists(view);
+    }
+
+    private void doctorLists(View rootView) {
+        //todo: sort by suggested doctor
+        RecyclerView recyclerView;
+        List<DoctorRegistrationModel> doctorDetails = new ArrayList<>();
+
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference(Utils.DOCTORS_REF);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    DoctorRegistrationModel doctorHelper = snapshot.getValue(DoctorRegistrationModel.class);
+
+                    if (doctorHelper != null) {
+                        doctorDetails.add(doctorHelper);
+
+                        DoctorListAdapter adapter = new DoctorListAdapter(doctorDetails, getContext());
+                        recyclerView.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled: ", databaseError.toException());
+            }
+        });
     }
 }
