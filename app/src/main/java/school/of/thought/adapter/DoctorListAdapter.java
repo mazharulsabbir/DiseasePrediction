2e73fe4ab11b19e8484ebbf23e7d10b3501bf547
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-01-06 16:01:54 +0600

    disease result with doctor list # search doctors

--- app/src/main/java/school/of/thought/adapter/DoctorListAdapter.java
+++ app/src/main/java/school/of/thought/adapter/DoctorListAdapter.java
@@ -1,6 +1,7 @@
 package school.of.thought.adapter;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
